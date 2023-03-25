98cd3270a2ca61cead4e3accff9b98f8fa5b390f
Kheyali15 <kheyalidhara15@gmail.com>
2023-03-25 15:36:59 +0530

    Commiting a file

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+let temprature = () => {
+    let opt = document.querySelector("#unit");
+    let display = document.querySelector(".value");
+    var a = document.querySelector(".take_val").value;
+    if (opt.value == "Celsius") {
+        console.log(a, "C to F", opt.value)
+        let F = a * (9 / 5) + 32
+        display.innerHTML = F.toFixed(4) + " F"
+    } else {
+        console.log(a, "F to C", opt.value)
+        let C = (a - 32) * (5 / 9)
+        display.innerHTML = C.toFixed(4) + " ºC"
+    }
+}
